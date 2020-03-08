6b355f2c581f0f932c7050116e9f40911da8a055
pixeldasher <phillip.weidinger@mailbox.tu-dresden.de>
2020-03-08 12:16:12 +0100

    Moved code to its corresponding module, cleaned up a little bit

--- src/main.py
+++ src/main.py
@@ -1,65 +1,5 @@
 #!/usr/bin/env python3
 
-import ev3dev.ev3 as ev3
-import logging
-import os
-import paho.mqtt.client as mqtt
-import uuid
-import time
-
-import odometry
-from communication import Communication
-from odometry import Odometry
-from planet import Direction, Planet
-
-client = None  # DO NOT EDIT
-
-
-def run():
-    # DO NOT CHANGE THESE VARIABLES
-    #
-    # The deploy-script uses the variable "client" to stop the mqtt-client after your program stops or crashes.
-    # Your script isn't able to close the client after crashing.
-    global client
-
-    client = mqtt.Client(client_id=str(uuid.uuid4()),  # Unique Client-ID to recognize our program
-                         clean_session=False,  # We want to be remembered
-                         protocol=mqtt.MQTTv31  # Define MQTT protocol version
-                         )
-    log_file = os.path.realpath(__file__) + '/../../logs/project.log'
-    logging.basicConfig(filename=log_file,  # Define log file
-                        level=logging.DEBUG,  # Define default mode
-                        format='%(asctime)s: %(message)s'  # Define default logging format
-                        )
-    logger = logging.getLogger('RoboLab')
-
-    # THE EXECUTION OF ALL CODE SHALL BE STARTED FROM WITHIN THIS FUNCTION.
-    # ADD YOUR OWN IMPLEMENTATION HEREAFTER.
-
-ts = ev3.TouchSensor()
-ts.value()
-us = ev3.UltrasonicSensor()
-us.mode = 'US-DIST-CM'
-distance = us.value()
-cs = ev3.ColorSensor()
-ev3.Sound.beep()
-cs.mode = 'COL-COLOR'
-m = ev3.LargeMotor("outA")
-m2 = ev3.LargeMotor("outB")
-
-while True:
-     odometry.colorcheck(cs.value())
-     odometry.distancecheck(us.distance_centimeters)
-     odometry.touchcheck(ts.value())
-
-
-# DO NOT EDIT
-if __name__ == '__main__':
-    run()
-
-#!/usr/bin/env python3
-
-
 import logging
 import os
 import paho.mqtt.client as mqtt
@@ -102,7 +42,7 @@ def run():
     comm = Communication(client, logger)
     comm.send_ready_message()
     """
-
+    
 
 # Define sensors
 ts = ev3.TouchSensor()
@@ -120,53 +60,22 @@ motor_left = ev3.LargeMotor("outA")
 motor_right = ev3.LargeMotor("outB")
 
 
-# Define movement functions
-def move_straight():
-    motor_left.speed_sp = 250
-    motor_right.speed_sp = 250
-    motor_left.command = "run-forever"
-    motor_right.command = "run-forever"
-
-    
-def move_left():
-    motor_left.speed_sp = 125
-    motor_right.speed_sp = 250
-    motor_left.command = "run-forever"
-    motor_right.command = "run-forever"
-
-    
-def move_right():
-    motor_left.speed_sp = 250
-    motor_right.speed_sp = 125
-    motor_left.command = "run-forever"
-    motor_right.command = "run-forever"
-
-    
-def stop_moving():
-    motor_left.speed_sp = 0
-    motor_right.speed_sp = 0
-    motor_left.command = "run-forever"
-    motor_right.command = "run-forever"
-
-
-def turn_around():
-    motor_left.speed_sp = 400
-    motor_right.speed_sp = -400
-    motor_left.command = "run-forever"
-    motor_right.command = "run-forever"
-    time.sleep(1.075)
-        
-
-
-# Define phases
-def check_phase():
-    odometry.movement_check(odometry.touch_check(ts.value()) & odometry.distance_check(us.distance_centimeters))
-
-
 # System loop for running through all phases
 def system_loop():
+    wheel_left = []
+    wheel_right = []
+
     while True:
-        check_phase()
+        odometry.check_phase()
+        motor_left_value = (motor_left.position / 360)
+        motor_right_value = (motor_right.position / 360)
+        wheel_left.append(format(motor_left_value, ".2f"))
+        wheel_right.append(format(motor_right_value, ".2f"))
+        #print(wheel_left)
+        #print(wheel_right)
+        time.sleep(0.05)
+
+
 
 
 # DO NOT EDIT
--- src/odometry.py
+++ src/odometry.py
@@ -2,6 +2,7 @@
 
 # Attention: Do not import the ev3dev.ev3 module in this file
 import main
+import time
 
 
 class Odometry:
@@ -15,27 +16,21 @@ class Odometry:
     
 path_dict = {}
 
-def follow_line():
-    red = main.cs.color.getRed()
-    blue = main.color.getBlue()
-    green = main.color.getGreen()
-
-
 def touch_check(n):
     if (n == 1):
         path_dict.update({"latest_path" : "blocked t"})
         print(path_dict)
-        main.stop_moving()
+        stop_moving()
         return False
     else:
         return True
 
 
 def distance_check(d):
-    if (d < 20):
+    if (d < 25):
         path_dict.update({"latest_path" : "blocked d"})
         print(path_dict)
-        main.turn_around()
+        turn_around()
         return False
     else:
         return True
@@ -45,4 +40,71 @@ def movement_check(boolean):
     if boolean:
         follow_line()
     else:
-        main.stop_moving()
+        stop_moving()
+
+
+# Define movement functions
+def move_straight():
+    main.motor_left.speed_sp = 250
+    main.motor_right.speed_sp = 250
+    main.motor_left.command = "run-forever"
+    main.motor_right.command = "run-forever"
+
+    
+def move_left():
+    main.motor_left.speed_sp = 25
+    main.motor_right.speed_sp = 250
+    main.motor_left.command = "run-forever"
+    main.motor_right.command = "run-forever"
+
+    
+def move_right():
+    main.motor_left.speed_sp = 250
+    main.motor_right.speed_sp = 25
+    main.motor_left.command = "run-forever"
+    main.motor_right.command = "run-forever"
+
+    
+def stop_moving():
+    main.motor_left.speed_sp = 0
+    main.motor_right.speed_sp = 0
+    main.motor_left.command = "run-forever"
+    main.motor_right.command = "run-forever"
+
+
+def luminance():
+    current_value_red = (main.cs.bin_data("hhh")[0] / 185)
+    current_value_green = (main.cs.bin_data("hhh")[1] / 321)
+    current_value_blue = (main.cs.bin_data("hhh")[2] / 157)
+    value = (0.2126 * current_value_red + 0.7152 * current_value_green + 0.0722 * current_value_blue)
+
+    return value
+
+
+def turn_around():
+    main.motor_left.speed_sp = 400
+    main.motor_right.speed_sp = -400
+    main.motor_left.command = "run-forever"
+    main.motor_right.command = "run-forever"
+    time.sleep(0.25)
+    while (float(luminance()) > 0.4):
+        main.motor_left.speed_sp = 400
+        main.motor_right.speed_sp = -400
+        main.motor_left.command = "run-forever"
+        main.motor_right.command = "run-forever"
+    follow_line()
+
+
+def follow_line():
+    if (float(luminance()) < 0.4):
+        move_left()
+
+    if (0.4 >= float(luminance()) >= 0.7):
+        move_straight()
+
+    if (float(luminance()) > 0.7):
+        move_right()
+
+
+def check_phase():
+    movement_check(touch_check(main.ts.value()) & distance_check(main.us.distance_centimeters))
