317c0f1207ecf3471b6ac0d3fa07c20765394d8e
pixeldasher <phillip.weidinger@mailbox.tu-dresden.de>
2020-03-10 12:01:07 +0100

    Reworked the database and how it functions with other modules; Changed some code to work properly and to save the received data correctly; Added more comments.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
-
+# Module imports
 import logging
 import os
 import paho.mqtt.client as mqtt
 import uuid
 import ev3dev.ev3 as ev3
 import time
-import database
 
+# File imports
 import communication
 import odometry
 import planet
+import database
 
 client = None  # DO NOT EDIT
 
@@ -39,50 +40,69 @@ def run():
     # ADD YOUR OWN IMPLEMENTATION HEREAFTER.
     # Run the system loop for exploration
 
+    # Setup objects for classes inside modules
     global p
     global c
     p = planet.Planet()
     c = communication.Communication(client, logger)
 
+    # Run system loop for exploration
     system_loop()
 
 
-### 
-
-"""
-# Define sensors
-ts = ev3.TouchSensor()
+# Define sensor variables
 us = ev3.UltrasonicSensor()
 cs = ev3.ColorSensor()
 
-
-# Define sensor modes
+# Set sensor modes
 us.mode = 'US-DIST-CM'
 cs.mode = 'RGB-RAW'
 
-# Define motors
+# Assign motors to corresponding port on device
 motor_left = ev3.LargeMotor("outA")
 motor_right = ev3.LargeMotor("outB")
-"""
+
+# Default moving speed value
+database.motor_speed = 250
+
+
+# Function to push all the sensor data into the database
+def push_sensor_data():
+    database.color_sensor_red_raw = cs.bin_data("hhh")[0]
+    database.color_sensor_green_raw = cs.bin_data("hhh")[1]
+    database.color_sensor_blue_raw = cs.bin_data("hhh")[2]
+
+    database.color_sensor_red_rgb = cs.bin_data("hhh")[0] / 185
+    database.color_sensor_green_rgb = cs.bin_data("hhh")[1] / 321
+    database.color_sensor_blue_rgb = cs.bin_data("hhh")[2] / 157
+
+    database.ultra_sonic_sensor = us.distance_centimeters
 
 
 # System loop for exploration
 def system_loop():
     while True:
-        # Start odometry phase
-        """
-        odometry.check_phase()
-        odometry.station_fider()
-        """
+        # Add sensor data to database
+        push_sensor_data()
 
-        # Start communication phase
-        c.comm_phase_init()
+        # Send ready message to mothership, only works once
+        c.send_ready()
 
+        c.send_exploration_completed()
+
+        c.send_target_reached()
+
+        """
+        # Planet test code
+        # -----
+        # takes existing values from database and puts them in dicts, then adds them as new path as a whole
         start_dict = ((database.start_x, database.start_y), planet.Direction(database.start_dir))
         end_dict = ((database.end_x, database.end_y), planet.Direction(database.end_dir))
         this_path_weight = database.path_weight
 
+        # Combine all data
         p.add_path(start_dict, end_dict, this_path_weight)
+        """
 
         # Samplingrate for system loop ### 1/10 of a second
         time.sleep(1/10)
