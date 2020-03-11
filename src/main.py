90a3acda3bfb4c7f7f08aec10b7df6a25f57325a
pixeldasher <58001560+pixeldasher@users.noreply.github.com>
2020-03-11 16:00:46 +0100

    Added routine when brick is at station; Tweaked communication code; More comments

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python3
 
+# Import py modules
 import logging
+import uuid
 import os
 import paho.mqtt.client as mqtt
-import uuid
 import ev3dev.ev3 as ev3
-import time
+from time import sleep
+
+# Import src modules
 import odometry
 from communication import Communication
-#from odometry import Odometry
 from planet import Direction, Planet
-import database
+from database import Database
 
 client = None  # DO NOT EDIT
 
+# Define sensors
+us = ev3.UltrasonicSensor()
+cs = ev3.ColorSensor()
 
+# Define sensor modes
+us.mode = 'US-DIST-CM'
+cs.mode = 'RGB-RAW'
+
+# Define motors
+motor_left = ev3.LargeMotor("outA")
+motor_right = ev3.LargeMotor("outB")
+
+
+# Initial function for main
 def run():
     # DO NOT CHANGE THESE VARIABLES
     #
@@ -36,85 +51,77 @@ def run():
 
     # THE EXECUTION OF ALL CODE SHALL BE STARTED FROM WITHIN THIS FUNCTION.
     # ADD YOUR OWN IMPLEMENTATION HEREAFTER.
-    # Run the system loop for exploration
 
-    # Setup objects for classes inside modules
-    global p
-    global c
+    # Objects for classes of other modules
+    global p, c, d
     p = Planet()
     c = Communication(client, logger)
+    d = Database()
 
     # Run system loop for exploration
     system_loop()
 
-"""
-# Define sensor variables
-us = ev3.UltrasonicSensor()
-cs = ev3.ColorSensor()
 
-# Set sensor modes
-us.mode = 'US-DIST-CM'
-cs.mode = 'RGB-RAW'
-
-# Assign motors to corresponding port on device
-motor_left = ev3.LargeMotor("outA")
-motor_right = ev3.LargeMotor("outB")
-
-# Default moving speed value
-database.motor_speed = 250
-
-
-# Function to push all the sensor data into the database
+# Push all the sensor data into the database
 def push_sensor_data():
-    database.color_sensor_red_raw = cs.bin_data("hhh")[0]
-    database.color_sensor_green_raw = cs.bin_data("hhh")[1]
-    database.color_sensor_blue_raw = cs.bin_data("hhh")[2]
+    d.color_sensor_red_raw = cs.bin_data("hhh")[0]
+    d.color_sensor_green_raw = cs.bin_data("hhh")[1]
+    d.color_sensor_blue_raw = cs.bin_data("hhh")[2]
 
-    database.color_sensor_red_rgb = cs.bin_data("hhh")[0] / 185
-    database.color_sensor_green_rgb = cs.bin_data("hhh")[1] / 321
-    database.color_sensor_blue_rgb = cs.bin_data("hhh")[2] / 157
+    d.color_sensor_red_rgb = cs.bin_data("hhh")[0] / 185
+    d.color_sensor_green_rgb = cs.bin_data("hhh")[1] / 321
+    d.color_sensor_blue_rgb = cs.bin_data("hhh")[2] / 157
 
-    database.ultra_sonic_sensor = us.distance_centimeters
-"""  
-
-# Define sensors
-us = ev3.UltrasonicSensor()
-cs = ev3.ColorSensor()
-
-
-# Define sensor modes
-us.mode = 'US-DIST-CM'
-cs.mode = 'RGB-RAW'
-
-
-# Define motors
-motor_left = ev3.LargeMotor("outA")
-motor_right = ev3.LargeMotor("outB")
+    d.ultra_sonic_sensor = us.distance_centimeters
 
 
 # System loop for running through all phases
 def system_loop():
     odometry.start_driving()
     while True:
-        c.communication_phase()
+        # Add all sensor data to database
+        push_sensor_data()
 
-        """
-        odometry.colorscan()
-        """
+        # Movement function
         odometry.while_driving()
-        if odometry.move_smooth():
-            print("hallo")
-            print(odometry.stop_driving())
-            time.sleep(1)
-            motor_left.run_to_rel_pos(position_sp=270)
-            motor_right.run_to_rel_pos(position_sp=270)
-            time.sleep(3)
-            odometry.start_driving()
-        else:
-            pass
 
-        # Samplingrate for system loop ### 1/10 of a second
-        time.sleep(1/10)
+        # If stations has been reached:
+        if odometry.move_smooth():
+            # ... send ready message, only works at the first station
+            c.send_ready()
+
+            # ... move forward in order to not scan the same station twice
+            motor_left.run_to_rel_pos(position_sp=180)
+            motor_right.run_to_rel_pos(position_sp=180)
+            sleep(2.5)
+
+            # ... get data from odometry and save it to database
+            odometry.stop_driving()
+
+            # ... send collected data to mothership
+            c.send_path()
+            
+            # ... add the corrected path to planet data
+            # p.add_path(d.latest_path)
+
+            # ... replace old start coordinates and direction with current end coordinates and direction
+            d.update_start_coords()
+
+            # ... turn around to search for possible paths
+            # odometry.search_paths()
+
+            # ... send selected path choice to mothership
+            c.send_path_select
+
+            # ... make a sound
+            ev3.Sound.play_song((('D4', 'e3'),( 'D4', 'e3'), ('D4', 'e3'), ('G4', 'h')))
+
+            # ... continue driving
+            odometry.start_driving()
+
+
+        # Samplingrate for system loop ### 1/10 of a seconds
+        sleep(1/10)
 
 
 # DO NOT EDIT
