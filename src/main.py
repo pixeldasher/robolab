9f55e65d83e8ee4bd8f331f03ef6f4322399b28c
pixeldasher <phillip.weidinger@mailbox.tu-dresden.de>
2020-03-11 18:25:28 +0100

    Continued work on the system routine and added code on other modules

--- src/main.py
+++ src/main.py
@@ -9,24 +9,12 @@ import ev3dev.ev3 as ev3
 from time import sleep
 
 # Import src modules
-import odometry
+from odometry import Odometry
 from communication import Communication
 from planet import Direction, Planet
 from database import Database
 
 client = None  # DO NOT EDIT
-
-# Define sensors
-us = ev3.UltrasonicSensor()
-cs = ev3.ColorSensor()
-
-# Define sensor modes
-us.mode = 'US-DIST-CM'
-cs.mode = 'RGB-RAW'
-
-# Define motors
-motor_left = ev3.LargeMotor("outA")
-motor_right = ev3.LargeMotor("outB")
 
 
 # Initial function for main
@@ -53,71 +41,76 @@ def run():
     # ADD YOUR OWN IMPLEMENTATION HEREAFTER.
 
     # Objects for classes of other modules
-    global p, c, d
+    global p, c, d, o
     p = Planet()
     c = Communication(client, logger)
     d = Database()
+    o = Odometry()
 
     # Run system loop for exploration
     system_loop()
 
 
-# Push all the sensor data into the database
-def push_sensor_data():
-    d.color_sensor_red_raw = cs.bin_data("hhh")[0]
-    d.color_sensor_green_raw = cs.bin_data("hhh")[1]
-    d.color_sensor_blue_raw = cs.bin_data("hhh")[2]
-
-    d.color_sensor_red_rgb = cs.bin_data("hhh")[0] / 185
-    d.color_sensor_green_rgb = cs.bin_data("hhh")[1] / 321
-    d.color_sensor_blue_rgb = cs.bin_data("hhh")[2] / 157
-
-    d.ultra_sonic_sensor = us.distance_centimeters
-
-
 # System loop for running through all phases
 def system_loop():
-    odometry.start_driving()
-    while True:
-        # Add all sensor data to database
-        push_sensor_data()
+    o.start_driving()
 
-        # Movement function
-        odometry.while_driving()
+    while True:
+        # Add all sensor data to database# Movement function
+        o.while_driving()
 
         # If stations has been reached:
-        if odometry.move_smooth():
-            # ... send ready message, only works at the first station
+        if o.move_smooth():
+            # ... send ready message, only works once
             c.send_ready()
 
             # ... move forward in order to not scan the same station twice
-            motor_left.run_to_rel_pos(position_sp=180)
-            motor_right.run_to_rel_pos(position_sp=180)
-            sleep(2.5)
+            o.motor_left.speed_sp = 360
+            o.motor_right.speed_sp = 360
+            sleep(1)
 
             # ... get data from odometry and save it to database
-            odometry.stop_driving()
+            o.stop_driving()
 
             # ... send collected data to mothership
             c.send_path()
+
+            # ... turn around and check all directions for possible paths
+            #o.scan_directions()
+            o.turn_around(360)
+
+            # ... take all directions and put them int planet data
+            p.add_vertex(d.vert, d.directions)
             
             # ... add the corrected path to planet data
-            # p.add_path(d.latest_path)
+            p.add_path(d.latest_path_start, d.latest_path_end, d.latest_path_weight)
 
             # ... replace old start coordinates and direction with current end coordinates and direction
             d.update_start_coords()
 
-            # ... turn around to search for possible paths
-            # odometry.search_paths()
+            # ... select the next direction, check if the target has been reached
+            if p.select_direction((d.start_x, d.start_y), d.target) == None:
+                c.send_target_reached()
+            
+            if not p.select_direction((d.start_x, d.start_y), d.target) == None:
+                d.next_direction = p.select_direction((d.start_x, d.start_y), d.target)
+            
+            # Check if exploration is completed
+            if p.explore_dict == {}:
+                c.send_exploration_completed()
 
             # ... send selected path choice to mothership
-            c.send_path_select
+            c.send_path_select()
 
-            # ... make a sound
+            # ... turn to the selected direction if one was given
+            if type(d.next_direction) == int:
+                o.turn_around((d.start_dir - d.next_direction) % 360)
+
+            # ... play a happy tune
             ev3.Sound.play_song((('D4', 'e3'),( 'D4', 'e3'), ('D4', 'e3'), ('G4', 'h')))
 
             # ... continue driving
-            odometry.start_driving()
+            o.start_driving()
 
 
         # Samplingrate for system loop ### 1/10 of a seconds
