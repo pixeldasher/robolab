bb330e7750365e61bbde07607716e56f693e33ea
pixeldasher <phillip.weidinger@mailbox.tu-dresden.de>
2020-03-12 15:32:40 +0100

    Reworked database again, should work now; worked on system loop process;

--- src/database.py
+++ src/database.py
@@ -1,72 +1,69 @@
-# !/usr/bin/env python3
-
-# Attention: Do not import the ev3dev.ev3 module in this file
-# No imports
-
-class Database:
-    def __init__(self):
-        """
-        Initializes database module
-        """
-
-        # After the ready message's been sent, planet name is received
-        self.planet_name = None
+# After the ready message's been sent, planet name is received
+planet_name = None
 
 
-        # Temporarily saves the received message for further processing
-        self.received_message = None
+# Temporarily saves the received message for further processing
+received_message = None
 
 
-        # One time check at first station, should return as False afterwards
-        self.first_time_ready = True
+# One time check at first station, should return as False afterwards
+first_time_ready = True
 
 
-        # Type of the latest received message
-        self.message_type = None
+# Type of the latest received message
+message_type = None
 
 
-        # Boolean for whether or not the last message received an answer
-        self.answered = False
+# Boolean for whether or not the last message received an answer
+answered = False
 
 
-        # Saves a specific point in time, then compares it with the current time
-        self.time_offset = None
+# Saves a specific point in time, then compares it with the current time
+time_offset = None
 
 
-        # Variables for data extracted from mothership messages 
-        self.start_x = None
-        self.start_y = None
-        self.start_dir = None
+# Variables for data extracted from mothership messages 
+start_x = None
+start_y = None
+start_dir = None
 
-        self.end_x = None
-        self.end_y = None
-        self.end_dir = None
+end_x = None
+end_y = None
+end_dir = None
 
-        self.path_status = None
-        self.path_weight = None
+path_status = None
+path_weight = None
 
-        self.target = None
+target = None
 
-        self.done_message = None
-        self.testplanet_message = None
+done_message = None
+testplanet_message = None
 
-        # Variables for exploration on planet
-        self.next_direction = None
-        self.vert = (self.end_x, self.end_y)
-        self.directions = None
-
-
-        # Latest path, put together with the data above
-        self.latest_path_start = ((self.start_x, self.start_y), self.start_dir)
-        self.latest_path_end = ((self.end_x, self.end_y), self.end_dir)
-        self.latest_path_weight = self.path_weight
+# Variables for exploration on planet
+next_direction = None
+vert = (end_x, end_y)
+directions = None
 
 
-        # Booleans for whether or not the exploration is completed
-        self.exploration_completed = False
+# Latest path, put together with the data above
+latest_path_start = ((start_x, start_y), start_dir)
+latest_path_end = ((end_x, end_y), end_dir)
+latest_path_weight = path_weight
 
 
-    def update_start_coords(self):
-        self.start_x = self.end_x
-        self.start_y = self.end_y
-        self.start_dir = (180 + self.end_dir) % 360
+# Booleans for whether or not the exploration is completed
+exploration_completed = False
+
+
+def update_start_coords():
+    global end_x
+    global start_x
+    global start_y
+    global start_dir
+    global end_dir
+    if type(end_x) == int:
+        start_x = end_x
+        start_y = end_y
+        start_dir = (180 + end_dir) % 360
+    else:
+        pass
